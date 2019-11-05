ea7198d88d1519667c1e9ef238423b08a8cf80d7
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-11-04 22:41:04 -0500

    tour start and end are dates and touring is boolean

--- db/migrate/20191105031441_create_bands.rb
+++ db/migrate/20191105031441_create_bands.rb
@@ -3,9 +3,9 @@ class CreateBands < ActiveRecord::Migration[6.0]
     create_table :bands do |t|
       t.string :name
       t.string :genre
-      t.string :touring
-      t.string :tour_start
-      t.string :tour_end
+      t.boolean :touring
+      t.date :tour_start
+      t.date :tour_end
 
       t.timestamps
     end
