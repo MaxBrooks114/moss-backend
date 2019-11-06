7c90705200ff42aa7dd00578e051fc8711043c87
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-11-05 21:29:08 -0500

    ran migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,75 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_11_05_223813) do
+
+  create_table "bands", force: :cascade do |t|
+    t.string "name"
+    t.string "genre"
+    t.boolean "touring"
+    t.date "tour_start"
+    t.date "tour_end"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "concerts", force: :cascade do |t|
+    t.string "name"
+    t.date "date"
+    t.boolean "opener"
+    t.integer "venue_id"
+    t.integer "band_id"
+    t.integer "location_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "concertsusers", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "concert_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string "city"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.float "score"
+    t.float "venue"
+    t.float "sound"
+    t.float "performance"
+    t.float "set"
+    t.float "price"
+    t.text "write_up"
+    t.integer "user_id"
+    t.integer "concert_id"
+    t.integer "venue_id"
+    t.integer "band_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.string "password_digest"
+    t.integer "age"
+    t.integer "home_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "venues", force: :cascade do |t|
+    t.string "name"
+    t.integer "location_id"
+    t.string "description"
+    t.string "capacity"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
