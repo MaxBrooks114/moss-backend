cd346dd2a3015b19fb8537e8d86f798d93b65931
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-11-10 10:36:29 -0500

    serialize user data

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,18 +11,22 @@ class Api::V1::UsersController < ApplicationController
   # GET /users/1
   def show
     user_json = UserSerializer.new(@user).serialized_json
+    @concerts = current_user.concerts
    render json: user_json
   end
 
   # POST /users
   def create
     @user = User.new(user_params)
-
     if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
+        session[:user_id] = @user.id
+        render json: UserSerializer.new(@user), status: :created
+      else
+        resp = {
+          error: @user.errors.full_messages.to_sentence
+        }
+        render json: resp, status: :unprocessable_entity
+      end
   end
 
   # PATCH/PUT /users/1
@@ -47,6 +51,6 @@ class Api::V1::UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:name, :username, :password_digest, :age, :home)
+      params.require(:user).permit(:name, :username, :password, :age, :home)
     end
 end
