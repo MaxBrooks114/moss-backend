be42b2c22f8f68038acb75c0d7d81a41472ee2db
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-11-10 10:36:40 -0500

    combined review score instance method

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -3,4 +3,16 @@ class Concert < ApplicationRecord
   has_many :users, through: :concertsusers
   has_many :reviews
 
+
+  def combined_review_score
+    combined_score = 0
+    if self.reviews.count > 0
+      self.reviews.each do |review|
+        combined_score += review.final_score
+      end
+      combined_score = combined_score / self.reviews.count
+    end
+      return combined_score
+  end
+
 end
