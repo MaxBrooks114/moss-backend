be7fad19238ee9abb5659f20d0271c91931c0cbb
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-11-10 10:36:52 -0500

    review validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,13 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :concert
+  validates :concert_id, uniqueness: { scope: :user_id, message: "You've already review this concert!" }
+
+
+  def final_score
+     final_score = (venue_score + sound_score + performance_score + set_score)/4
+  end
+
+
+
 end
