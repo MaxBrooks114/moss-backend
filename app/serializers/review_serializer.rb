d864fd1ccd5073685d935b5488da699c442a4116
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-11-11 21:23:16 -0500

    added user and cocnert info to review serializer

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,8 +1,21 @@
 class ReviewSerializer
   include FastJsonapi::ObjectSerializer
   attributes  :final_score, :venue_score, :sound_score, :performance_score, :set_score, :price, :write_up
-  belongs_to :user
-  belongs_to :concert
+  attribute :user do |review|
+    review.user do |user|
+      {
+        username: user.username,
 
-  
+      }
+    end
+
+  end
+  attribute :concert do |review|
+    review.concert do |concert|
+      {
+        name: concert.name
+      }
+    end
+  end
+
 end
