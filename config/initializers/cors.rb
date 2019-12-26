9b6a862610803d6b9471e6c2986f3ca8d2477d1d
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-12-26 10:30:15 -0500

    whitelist http for heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -15,6 +15,14 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
+    origins 'http://moss.herokuapp.com'
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+
+  allow do
     origins  'http://localhost:3001'
 
     resource '*',
